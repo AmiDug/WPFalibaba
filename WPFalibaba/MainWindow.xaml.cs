be40755442084fb5ac6b997eb8432c9d0870e73f
Amiran Dugiev <dugiev123@gmail.com>
2022-12-09 10:31:37 +0100

    added button and label

--- WPFalibaba/MainWindow.xaml.cs
+++ WPFalibaba/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFalibaba
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = "button pressed!";
+        }
     }
 }
