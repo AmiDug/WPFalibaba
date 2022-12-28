4cd90d18784f1d6f9832b8507855e036e2edb732
Amiran Dugiev <dugiev123@gmail.com>
2022-12-28 17:17:36 +0100

    finally fixed we are stupid

--- WPFalibaba/ViewModel.cs
+++ WPFalibaba/ViewModel.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections.Generic;
-
-public class ViewModel
+namespace WPFalibaba
 {
-    public List<Person> Data { get; set; }
-
-    public ViewModel()
+    public class ViewModel
     {
-        Data = new List<Person>()
+        public List<Person> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new List<Person>()
             {
                 new Person { Name = "David", Height = 180 },
                 new Person { Name = "Michael", Height = 170 },
                 new Person { Name = "Steve", Height = 160 },
                 new Person { Name = "Joel", Height = 182 }
             };
+        }
     }
 }
