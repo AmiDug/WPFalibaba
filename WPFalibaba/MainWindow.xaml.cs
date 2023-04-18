5be7129e2e242b09560f0ea85d2885748a531e12
Amiran Dugiev <dugieva@hotmail.com>
2023-04-18 22:25:57 +0200

    Final Version

--- WPFalibaba/MainWindow.xaml.cs
+++ WPFalibaba/MainWindow.xaml.cs
@@ -3,9 +3,6 @@ using System.Windows.Controls;
 
 namespace WPFalibaba
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
